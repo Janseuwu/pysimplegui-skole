0df3be1629e42e0a0e1690260c9e56d9bc9c1a94
Janseuwu <101401703+Janseuwu@users.noreply.github.com>
2022-11-09 13:28:05 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+import PySimpleGUI as sg
+
+# launches a window with a text editor
+def txt_editor():
+
+    menu_def = [
+        ['File',['New','Open','Save','Save As','---','Exit']],
+        ['Edit',['Undo','---','Cut','Copy','Paste','Delete','---','Find...','Replace...','---','Select All']],
+        ]
+
+    layout = [
+        [sg.Menu(menu_definition=menu_def)],
+        [sg.Multiline(size=(100, 40))]
+    ]
+    title = 'Text file'
+
+    # create our text editor window
+    window = sg.Window(title, layout, resizable=True)
+
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED:
+            break
+        
+password = 'gaide gad' # ideally we should use dotenv with some hashing
+title = 'Login'
+layout = [
+    [sg.Text('Insert password')],
+    [sg.InputText(password_char='*')],
+    [sg.Button('Cancel'), sg.Sizer(207), sg.Button('Submit')]
+]
+
+# create our password window
+window = sg.Window(title, layout)
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Cancel':
+        break
+
+    # checks if password matches the set password
+    if event == 'Submit':
+        if values[0] == password: 
+            window.close()
+            txt_editor() # calls function that opens the text editor
+            
